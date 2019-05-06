7cb7333fb37aef626d4d6b3dae85aa053946823e
suman kumar ghosh <sumanghoshmca@gmail.com>
2019-05-06 20:31:53 +0530

    complete video 9

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
 
 // Item Model
 const Item = require('../../models/Item');
@@ -16,7 +17,7 @@ router.get('/', (req, res) => {
 //@route    POST api/items
 //@desc     Create An Item
 //@access   public
-router.post('/', (req, res) => {
+router.post('/', auth, (req, res) => {
     const newItem = new Item({
         name: req.body.name
     });
@@ -26,7 +27,7 @@ router.post('/', (req, res) => {
 //@route    DELETE api/items/:id
 //@desc     Delete An Item
 //@access   public
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, (req, res) => {
     Item.findById(req.params.id)
         .then(item => item.remove().then(() => res.json({success: true})))
         .catch(err => res.status(404).json({success: false}));
