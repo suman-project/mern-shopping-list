e0a1dcb20972583a17feb5fd1d74cc8da963ed5f
suman kumar ghosh <sumanghoshmca@gmail.com>
2019-05-11 22:48:55 +0530

    complete video 10

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import itremReducer from './itemReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
-  item: itremReducer
+  item: itremReducer,
+  error: errorReducer,
+  auth: authReducer
 });
--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next) {
   const token = req.header('x-auth-token');
 
   // Check for token
-  if(!token) res.status(401).json({msg: 'No Token, authorization denied'});
+  if(!token) return res.status(401).json({msg: 'No Token, authorization denied'});
 
   try {
     //Verify Token
